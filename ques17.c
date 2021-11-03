8017a7216ca0021a8c9b70208534ba1e341d9722
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-03 23:17:40 +0530

    Add files via upload

---
+++ ques17.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,pare;
+	printf("enter the first no.\n",a);
+	scanf("%d",&a);
+	printf("enter the second no.\n",b);
+	scanf("%d",&b);
+	pare=2*(a+b);
+	printf("parameter of rectangle is %d",pare);
+	return 0;
+	
+}
