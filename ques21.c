95d03128e5178b88d581ce4bc49ae0d2693e3d51
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-03 23:19:10 +0530

    Add files via upload

---
+++ ques21.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float cel,frah;
+	printf("enter temperature in celcius\n");
+	scanf("%f",&cel);
+	frah=cel*(9.0/5.0)+32;
+	printf("the temp. in frah is %f",frah);
+	return 0;
+	
+	
+}
