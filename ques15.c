73fd91d0cfdf3f02db4d5f32bae7d442c74b605e
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-03 23:16:56 +0530

    Add files via upload

---
+++ ques15.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the first no.\n");
+	scanf("%d",&a);
+	int b;
+	printf("enter the second no.\n");
+	scanf("%d",&b);
+	int sum;
+	sum=(a+b);
+	printf("the sum of two no. is %d", sum);
+	return 0;
+}
