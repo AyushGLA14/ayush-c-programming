29cf46a07a2b4f39f7695d60664aa57da4c9172a
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-03 23:18:02 +0530

    Add files via upload

---
+++ ques18.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,area;
+	printf("enter the first no.\n",a);
+	scanf("%d",&a);
+	printf("enter the second no.\n",b);
+	scanf("%d",&b);
+	area=a*b;
+	printf("the area of rectangle is %d",area);
+	return 0;
+}
