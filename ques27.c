d9b15eb68d12bad49b73327d41e7c227e0138853
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-07 22:42:37 +0530

    Add files via upload

---
+++ ques27.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int b,h,area;
+	printf("the base of triangle is\n");
+	scanf("%d",&b);
+	printf("the height of triangle is\n");
+	scanf("%d",&h);
+	area=(b*h)/2;
+	printf("the area of triangle is%d",area);
+	return 0;
+}
