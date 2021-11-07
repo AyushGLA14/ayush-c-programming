31af2941429a8811263c0b95dedc6444138701b5
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-07 22:44:13 +0530

    Add files via upload

---
+++ ques28.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float area;
+	printf("enter the side of eq. triangle\n");
+	scanf("%d",&a);
+	area=(1.73*a*a)/4;
+	printf("the area of eq. triangle is %f",area);
+	return 0;
+}
