24a39bd3c7773d7a63c382bad3cd10c739a764d6
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-07 22:41:05 +0530

    Add files via upload

---
+++ ques25.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+	int x,num;
+	printf("enter the no.\n");
+	scanf("%d",&x);
+	num=sqrt(x);
+	printf("the square root of no. is %d",num);
+	return 0;
+}
