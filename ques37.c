104ac1079808609c3615b37c7caaaf9a48fc3199
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-12 15:09:09 +0530

    Add files via upload

---
+++ ques37.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float value=1234.67566f;
+	printf("value: %f(using %%f format specifier)\n",value);
+	printf("value: %e(using %%e format specifier)\n",value);
+	return 0;
+	
+}
