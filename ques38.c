6549be96e5ff253feb76e46ae062b0bf09193f3d
AyushGLA14 <91405635+AyushGLA14@users.noreply.github.com>
2021-11-12 15:09:50 +0530

    Add files via upload

---
+++ ques38.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int pop=30000;
+	float pop1,pop2,tpop;
+	pop1=30000+(30000*20.0)/100.0;
+	pop2=30000+(30000*30.0)/100.0;
+	tpop=pop1+pop2;
+	printf("the total population is %f",tpop);
+	return 0;
+	
+}
